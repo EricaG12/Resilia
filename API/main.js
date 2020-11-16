0708b1b56240ee433d75429c3f54034a0224118a
EricaG12 <ericagm@id.uff.br>
2020-11-16 02:22:56 -0300

    organizando pastas e subindo tarefas

---
+++ API/main.js
@@ -0,0 +1,13 @@
+let buttonRandom = document.querySelector('#buttonRandom')
+
+buttonRandom.addEventListener('click', Controller.mostraConselhoAleatorio) 
+buttonRandom.addEventListener('click', Controller.mostraConselhoAleatorio)
+
+let form = document.querySelector('#form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    let inputSearch = document.querySelector('#inputSearch')
+    let value = inputSearch.value
+    Controller.mostrarConselhoPorBusca(value)
+}) 
